735a09d05591d9c17f805c8521362aa7d7251cc6
Mehdi.Zare <mehdi.zare@nav.no>
2024-12-16 08:50:39 +0100

    logo

--- lps-client-frontend/src/App.js
+++ lps-client-frontend/src/App.js
@@ -1,19 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { CssBaseline, Container, Typography } from '@mui/material';
+import {CssBaseline, Container, Typography, Box} from '@mui/material';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LoginForm from './LoginForm';
 import FiltererInntektsmeldinger from './FiltererInntektsmeldinger';
 import Velkommen from './Velkommen';
+import logo from './logo.png'; // Adjust the path if necessary
 
 function App() {
   return (
     <Router>
       <Container>
         <CssBaseline />
-        <Typography variant="h4" align="center" gutterBottom>
-          TigerSys
-        </Typography>
+        <Box display="flex" justifyContent="center" mt={4}>
+          <img src={logo} alt="Logo" style={{maxWidth: '150px'}}/>
+        </Box>
+
         <Typography variant="subtitle1" align="center" gutterBottom>
           Ditt lønns- og personalsystem
         </Typography>
