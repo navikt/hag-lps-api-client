ba222632796ebac7b6a8f3a780823a461cb069a3
Jesper Hustad <jesper.forrest.hustad@nav.no>
2024-12-19 16:48:23 +0100

    lagt til alpha Vite versjon

---
+++ lps-client-frontend-v/src/App.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import {CssBaseline, Container, Typography, Box} from '@mui/material';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import LoginForm from './LoginForm';
+import FiltererInntektsmeldinger from './FiltererInntektsmeldinger';
+import Velkommen from './Velkommen';
+import logo from './logo.png'; // Adjust the path if necessary
+
+function App() {
+    return (
+        <Router>
+            <Container>
+                <CssBaseline/>
+                <Box display="flex" justifyContent="center" mt={4}>
+                    <img src={logo} alt="Logo" style={{maxWidth: '150px'}}/>
+                </Box>
+
+                <Typography variant="subtitle1" align="center" gutterBottom>
+                    Ditt lønns- og personalsystem
+                </Typography>
+                <Routes>
+                    <Route path="/" element={<LoginForm/>}/>
+                    <Route path="/search" element={<FiltererInntektsmeldinger/>}/>
+                    <Route path="/velkommen" element={<Velkommen/>}/>
+                </Routes>
+            </Container>
+        </Router>
+    );
+}
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(<App/>);
+
+export default App;
