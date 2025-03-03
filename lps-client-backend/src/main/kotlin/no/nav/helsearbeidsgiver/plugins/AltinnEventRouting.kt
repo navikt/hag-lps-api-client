b6660fcc178a238d92fec27ec329f2528b28cf1c
Magnus Sælensminde <magnus.salensminde@bekk.no>
2025-03-03 14:26:03 +0100

    Fjern unødvendig ip-ræl

--- lps-client-backend/src/main/kotlin/no/nav/helsearbeidsgiver/plugins/AltinnEventRouting.kt
+++ lps-client-backend/src/main/kotlin/no/nav/helsearbeidsgiver/plugins/AltinnEventRouting.kt
@@ -3,7 +3,6 @@ package no.nav.helsearbeidsgiver.plugins
 import io.ktor.http.HttpStatusCode
 import io.ktor.server.application.Application
 import io.ktor.server.application.call
-import io.ktor.server.plugins.origin
 import io.ktor.server.response.respond
 import io.ktor.server.routing.Routing
 import io.ktor.server.routing.post
@@ -19,13 +18,8 @@ fun Application.configureAltinnEventRouting() {
 private fun Routing.mottaDialogportenEvent() {
     post("/dialogporten-event") {
         try {
-            if (call.request.origin.remoteAddress != "20.100.24.41/32") {
-                logger().info("Mottok Dialogporten event fra en annen IP enn forventet")
-                call.respond(HttpStatusCode.Forbidden, "Forbidden")
-            } else {
-                logger().info("Mottok Dialogporten event fra forventet IP")
+            logger().info("Mottok Dialogporten event fra forventet IP")
                 call.respond(HttpStatusCode.OK)
-            }
         } catch (e: Exception) {
             logger().error("Feil ved mottak av Dialogporten event", e)
             call.respond(HttpStatusCode.InternalServerError, "Klarte ikke motta dialogporten event")
