cc80d675cae15a25ae99b412eb07332377abfcf4
Magnus Sælensminde <magnus.salensminde@bekk.no>
2025-03-04 08:54:44 +0100

    Legg på mer logging av altinnevent

--- lps-client-backend/src/main/kotlin/no/nav/helsearbeidsgiver/plugins/AltinnEventRouting.kt
+++ lps-client-backend/src/main/kotlin/no/nav/helsearbeidsgiver/plugins/AltinnEventRouting.kt
@@ -3,11 +3,15 @@ package no.nav.helsearbeidsgiver.plugins
 import io.ktor.http.HttpStatusCode
 import io.ktor.server.application.Application
 import io.ktor.server.application.call
+import io.ktor.server.request.receive
 import io.ktor.server.response.respond
 import io.ktor.server.routing.Routing
 import io.ktor.server.routing.post
 import io.ktor.server.routing.routing
+import kotlinx.serialization.Serializable
+import no.nav.helsearbeidsgiver.utils.AsStringSerializer
 import no.nav.helsearbeidsgiver.utils.logger
+import java.util.UUID
 
 fun Application.configureAltinnEventRouting() {
     routing {
@@ -18,7 +22,9 @@ fun Application.configureAltinnEventRouting() {
 private fun Routing.mottaDialogportenEvent() {
     post("/dialogporten-event") {
         try {
-            logger().info("Mottok Dialogporten event fra forventet IP")
+            logger().info("Mottok Dialogporten event.")
+            val dialogportenEvent = call.receive<DialogportenEvent>()
+            logger().info("Mottok Dialogporten event ${dialogportenEvent.tilLogString()}")
             call.respond(HttpStatusCode.OK)
         } catch (e: Exception) {
             logger().error("Feil ved mottak av Dialogporten event", e)
@@ -26,3 +32,21 @@ private fun Routing.mottaDialogportenEvent() {
         }
     }
 }
+
+@Serializable
+data class DialogportenEvent(
+    @Serializable(with = UuidSerializer::class)
+    val resourceinstance: UUID,
+    val type: String,
+    val resource: String,
+    val subject: String,
+    val source: String,
+) {
+    fun tilLogString(): String =
+        "DialogportenEvent(resourceinstance=$resourceinstance, type=$type, resource=$resource, subject=$subject, source=$source)"
+}
+
+object UuidSerializer : AsStringSerializer<UUID>(
+    serialName = "lps-client.kotlinx.UuidSerializer",
+    parse = UUID::fromString,
+)
